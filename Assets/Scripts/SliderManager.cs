22ea042736694967fbf5c80d4eacac84134a608b
Skyron <courtier.leo@gmail.com>
2024-11-23 20:17:34 +0100

    Slider

---
+++ Assets/Scripts/SliderManager.cs
@@ -0,0 +1,33 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderManager : MonoBehaviour
+{
+    private Slider _slider;
+    [SerializeField] Image fillImage;
+    [SerializeField] Gradient gradient;
+    [SerializeField, Range(1, 20)] private float strengthAnimation = 20f;
+    [SerializeField] private bool shake;
+    [SerializeField, Range(1, 10)] private float startAnimationDuration = 2f;
+    private RectTransform _rectTransform;
+    public float maxValue;
+
+    private void Start() {
+        _slider = GetComponent<Slider>();
+        _rectTransform = GetComponent<RectTransform>();
+        _slider.maxValue = maxValue;
+        _slider.DOValue(_slider.maxValue, startAnimationDuration);
+        fillImage.color = gradient.Evaluate(1f);
+    }
+    
+    public void SetSliderValue(float value) {
+        _slider.DOValue(value, .5f);
+        if(shake) _rectTransform.DOShakePosition(1f, strengthAnimation);
+        fillImage.DOColor(gradient.Evaluate(ConvertCurrentValueToPercent()), .5f);
+    }
+
+    private float ConvertCurrentValueToPercent() {
+        return _slider.value / _slider.maxValue;
+    }
+}
