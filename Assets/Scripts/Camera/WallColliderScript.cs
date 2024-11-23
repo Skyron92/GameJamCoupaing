196297ba15ab6e523bd16c0e332a46137b0a87b7
Justine Tristan <justine.tristan@yahoo.com>
2024-11-23 16:15:03 +0100

    FinishCamera

--- Assets/Scripts/Camera/WallColliderScript.cs
+++ Assets/Scripts/Camera/WallColliderScript.cs
@@ -7,6 +7,7 @@ public class WallColliderScript : MonoBehaviour
     [SerializeField] BrainCamera _brainCamera;
     public bool isVertical;
     public int WallSide;
+    public int WallTop;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,7 +25,7 @@ public class WallColliderScript : MonoBehaviour
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-          _brainCamera.FocusOnWall(isVertical, WallSide);
+          _brainCamera.FocusOnWall(isVertical, WallSide, WallTop);
         }
     }
 }
